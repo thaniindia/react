dd94113484f1e658a4b71682a6589b5cc95d70ba
Seiyial <seiyial@gmail.com>
2018-06-06 22:11:18 +0800

    Create some pages and page state

--- App.js
+++ App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, Alert } from 'react-native';
 
 export default class App extends React.Component {
-  
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
-  }
 
-  _onLongPressButton() {
-    Alert.alert('You long-pressed the button!')
+  constructor(props) {
+    super(props)
+    this.state = {
+      page: '0'
+    }
+  }
+  
+  navigate(number) {
+    this.setState({ page: number })
   }
 
   render() {
+    console.log('(*) this.state.page =>', this.state.page)
     return (
       <View style={{
         flex: 1,
@@ -20,31 +24,72 @@ export default class App extends React.Component {
       }}>
         <View style={{width: 50, height: 20}} />
 
-        <View style={{width: 415, height: 50, backgroundColor: 'white'}}> 
+        <View style={{width: '100%', height: 50, backgroundColor: 'white'}}> 
           <Text style={styles.style1}>திருக்குறள்</Text>
         </View>
 
-        <View style={{width: 415, height: 50, backgroundColor: 'skyblue'}} >
+        <View style={{width: '100%', height: 50, backgroundColor: 'skyblue'}} >
           <Text style={styles.style2}>திருக்குறள் : பால்</Text>
         </View>
 
-        <TouchableHighlight onPress={this._onPressButton}>
-          <View style={{width: 415, height: 50, backgroundColor: 'powderblue'}} >
-            <Text style={styles.style3}>1. அறத்துப்பால்</Text>
-          </View>
-        </TouchableHighlight>
+        { this.state.page === '0' &&
+            <React.Fragment>
+              <TouchableHighlight onPress={() => this.navigate('1')}>
+                <View style={{width: '100%', height: 50, backgroundColor: 'powderblue'}} >
+                  <Text style={styles.style3}>1. அறத்துப்பால்</Text>
+                </View>
+              </TouchableHighlight>
 
-        <TouchableOpacity onPress={this._onPressButton}>
-          <View style={{width: 415, height: 50, backgroundColor: 'powderblue'}} >
-            <Text style={styles.style3}>2. பொருட்பால்</Text>
-          </View>
-        </TouchableOpacity>
+              <TouchableHighlight onPress={() => this.navigate('2')}>
+                <View style={{width: '100%', height: 50, backgroundColor: 'powderblue'}} >
+                  <Text style={styles.style3}>2. பொருட்பால்</Text>
+                </View>
+              </TouchableHighlight>
 
-        <TouchableWithoutFeedback onPress={this._onPressButton}>
-          <View style={{width: 415, height: 50, backgroundColor: 'powderblue'}} >
-            <Text style={styles.style3}>3. காமத்துப்பால்</Text>
-          </View>
-        </TouchableWithoutFeedback>
+              <TouchableHighlight onPress={() => this.navigate('3')}>
+                <View style={{width: '100%', height: 50, backgroundColor: 'powderblue'}} >
+                  <Text style={styles.style3}>3. காமத்துப்பால்</Text>
+                </View>
+              </TouchableHighlight>
+            </React.Fragment>
+        }
+        
+        {
+          this.state.page === '1' &&
+          <React.Fragment>
+            <View style={{ flex: 1, flexDirection: 'row', maxHeight: 50, height: 50, backgroundColor: 'skyblue'}}>
+              <View style={{ width: 100, height: 50, backgroundColor: 'cornflowerblue'}} >
+                <TouchableHighlight onPress={() => this.navigate('0')}>
+                  <View style={{ width: '100%', height: '100%' }}>
+                    <Text style={styles.style3}>Back</Text>
+                  </View>
+                </TouchableHighlight>
+              </View>
+
+              <View style={{ flex: 1, height: 50, backgroundColor: 'cadetblue'}} >
+                <Text style={styles.style3}>1. அறத்துப்பால்</Text>
+              </View>
+            </View>
+
+            <TouchableHighlight onPress={() => this.navigate('1-1')}>
+              <View style={{width: '100%', height: 50, backgroundColor: 'powderblue'}} >
+                <Text style={styles.style3}>1.1 அறத்துப்பால்</Text>
+              </View>
+            </TouchableHighlight>
+
+            <TouchableHighlight onPress={() => this.navigate('1-2')}>
+              <View style={{width: '100%', height: 50, backgroundColor: 'powderblue'}} >
+                <Text style={styles.style3}>1.2 பொருட்பால்</Text>
+              </View>
+            </TouchableHighlight>
+
+            <TouchableHighlight onPress={() => this.navigate('1-3')}>
+              <View style={{width: '100%', height: 50, backgroundColor: 'powderblue'}} >
+                <Text style={styles.style3}>1.3 காமத்துப்பால்</Text>
+              </View>
+            </TouchableHighlight>
+          </React.Fragment>
+        }
 
       </View>
     );
